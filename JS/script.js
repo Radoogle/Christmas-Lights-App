71b608e384a677bd6aca0c64bcc7277a62834e69
Radu Harangus <Radu.Harangus@gmail.com>
2020-06-30 14:37:02 +0300

    animation with turn on/off

--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,8 @@
 /* jshint esversion: 6 */
 let ROW_NUMBER = 0;
+const listItems = document.getElementsByTagName('li');
+const TurnOnButton = document.getElementById('TurnOnButton');
+const TurnOffButton = document.getElementById('TurnOffButton');
 
 function makeRowVisible() {
   if (ROW_NUMBER < 7) {
@@ -20,3 +23,22 @@ function hideRow() {
     alert('There are no more lightropes to remove!');
   }
 }
+
+TurnOnButton.addEventListener('click', () => {
+  // listItems[0].style.animationIterationCount = "infinite";
+  // console.log(listItems[0].style.animationIterationCount);
+  // listItems.map(li => li.style.animationIterationCount = infinite);
+  for (let i = 0; i < listItems.length; i++) {
+    listItems[i].style.animationIterationCount = "infinite";
+  }
+});
+
+TurnOffButton.addEventListener('click', () => {
+  // alert('turn off: ' + listItems[0].style.animationIterationCount);
+  // listItems[0].style.animationIterationCount = "0";
+  // console.log(listItems[0].style.animationIterationCount);
+  // listItems.map(li => li.style.animationIterationCount = 0);
+  for (let i = 0; i < listItems.length; i++) {
+    listItems[i].style.animationIterationCount = "0";
+  }
+});
